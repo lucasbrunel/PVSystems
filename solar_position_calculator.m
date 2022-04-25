a990d62f68dcce53d0bc772e7e745f98ff82a0cf
lucasbrunel <65372497+lucasbrunel@users.noreply.github.com>
2022-04-25 12:08:34 +0200

    Add files via upload

--- solar_position_calculator.m
+++ solar_position_calculator.m
@@ -1,3 +1,31 @@
 %Creating a Solar position Calculator
-x=8;
-test = "Hello world";
+%{
+The goal of this assignment is to plot sun paths for specifics dates and analemmas for
+given time instants in an arbitrary location. First, you need to be able to calculate
+the sun position (in terms of altitude/elevation/zenith and azimuth) given a time
+instant and a pair of geographical coordinates.
+%}
+
+%% ----- Base variables ------ %%
+Pressure = 101325; %Pa
+Temp = 12; %Degrees C
+
+%Sun location
+location.longitude = 0; %Degrees, from the equator (+ve North)
+location.latitude = 0; %Degrees, from meridian (+ve East)
+
+%Time with reference of
+time.year = 0;
+time.day = 0;
+time.hour = 0;
+time.minute = 0;
+time.second = 0;
+
+%% ------ Calculations ------ %%
+
+
+
+
+%% ------ Final output ------- %%
+azimuth_ang = 0; %Degrees from North (East = 90degrees)
+elevation_ang = 0; %Degrees from the horizon (-ve is below horizon)x1
