2a3eb99206d4cab725ef6f48793563fb99e0e45a
lucasbrunel <65372497+lucasbrunel@users.noreply.github.com>
2022-05-04 14:38:07 +0200

    Add files via upload

--- assignment_3.m
+++ assignment_3.m
@@ -8,8 +8,7 @@ clear all
 eff = 0.18; %Efficiency of the panel
 R = 0.1; %Reflectivity
 t_inoct = 54 + 273; %Temperature in INOCT conditions [Kelvin]
-beta = 1/t_inoct;
-theta_m = deg2rad(40); %Angle of the panel [degrees]
+theta_m = deg2rad(30); %Angle of the panel [degrees]
 length = 1.5; %Length of the panel [m]
 width = 1; %Width of the panel [m]
 epsilon_t = 0.84; %Emissivity of the top
@@ -18,6 +17,8 @@ epsilon_b = 0.89; %Emissivity of the back
 %Environmental conditions
 Gm = 750; %Irradiance [W m-2]
 Ta = 20+273; %Ambient Temperature [Kelvin]
+tsky = 0.0552*(Ta^(3/2)); %Temperature of the sky
+beta = 1/Ta;
 w = 4; %Wind speed [m s-1]
 
 %Air Propoerties and Other Consts
@@ -40,10 +41,16 @@ visc_k = mu_air/roh_air; %Kinematic viscosity
 
 Re = (w*Dh)/visc_k; %Reynolds number
 
-h_forced_lam = ((0.86*(Re^-0.5))/(pr_air^0.67))*roh_air*cp_air * w;
+h_forced_lam = ((0.86*(Re^-0.5))/(pr_air^0.67))*roh_air*cp_air*w; % Something wrong
 h_forced_turb = ((0.028*(Re^-0.2))/(pr_air^0.4))*roh_air*cp_air * w;
 
-Gr = ((g*beta*(t_inoct - Ta)*(Dh^3))/w^2)*sin(theta_m);% Is w really wind speed?
+if w >= 3
+    h_forced = h_forced_turb;
+else
+    hforced = h_forced_lam;
+end
+
+Gr = ((g*beta*(t_inoct - Ta)*(Dh^3))/visc_k^2)*sin(theta_m);
 
 if Gr <= 0
     Gr = 0;
@@ -51,9 +58,11 @@ end
 
 h_free = (0.21*((Gr*pr_air)^0.32)*k_air)/Dh;
 
-h_top = ((h_forced_lam^3) * (h_free^3))^(1/3); %Using Laminar turbulence
-%Above gives a complex number...
+h_top = ((h_forced^3) * (h_free^3))^(1/3);
 
+justR = ((alpha*Gm) - (h_top*(t_inoct-Ta)) - (epsilon_t*sigma_b*((t_inoct^4)-(tsky^4)))) / (h_top*(t_inoct - Ta)) + (epsilon_b*sigma_b*((t_inoct^4)-(tsky^4)));
+
+h_bottom = R * h_top; % Is h_top the same?
 
 
 
