7eff46b7860375a27715dbfcf6a7a52b007bedc5
dsaunders9 <104137521+dsaunders9@users.noreply.github.com>
2022-05-24 13:59:57 +0200

    created main assignment file

---
+++ Main Assignment/main_assignment.m
@@ -0,0 +1,9 @@
+%Main Assignment 3
+%Santiago, Chile, Load profile 3
+
+%Task 1 - Load Profile
+load_profile = getLoadProfileA3(3);
+
+%Need to make bar plot of monthly demand
+
+%Task 2 - Irradiance Calculation
