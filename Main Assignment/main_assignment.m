f0699160ef3f80e72948eb82c25b4b5e571c11d8
dsaunders9 <104137521+dsaunders9@users.noreply.github.com>
2022-06-07 16:14:15 +0200

    deleted crap

--- Main Assignment/main_assignment.m
+++ Main Assignment/main_assignment.m
@@ -221,54 +221,6 @@ for i = 1:8
 end
 lands_total = sum(roof_irradiance_l);
 
-% %Irradiance Calculation
-% m_azim = [0, 90, 180, 270       
-%           0, 90, 180, 270];
-% m_tilt = [14.14111023, 14.14111023, 14.14111023, 14.14111023 
-%           59.28602106, 59.28602106, 59.28602106, 59.28602106];
-% m_irradiation_l = zeros(size(m_tilt));
-% m_irradiation_p = zeros(size(m_tilt));
-% skyline_landscape = load('landscape_skylines.mat');
-% skyline_portrait = load('portrait_skylines.mat');
-% 
-% for i = 1:2
-%     if i == 1
-%         skyline = skyline_landscape;
-%     elseif i == 2
-%         skyline = skyline_portrait;
-%     end
-%     for k = 1:4
-%         for j = 1:2
-%             sf = calculateShadingFactor(skyline.skylines{1}{1}, sun_azim, sun_alt);
-%             svf = svfCalculator(m_azim(1,k), m_tilt(j,1), skyline);
-%             G_dif = svf*DHI;
-%             G_ref = albedo*(1-svf)*GHI;
-%             cos_aoi = cosd(sun_Zen).*cosd(m_tilt(k))+sind(m_tilt(k)).*sind(sun_Zen).*cosd(sun_azim-m_azim(k));
-%             cos_aoi(cos_aoi<0) = 0;
-%             G_dir = DNI.*cos_aoi.*sf';
-%             if i == 1
-%                 m_irradiation_l(j,k) = sum(G_dir+G_dif+G_ref)/1e3;  %in kWh/m2
-%             elseif i == 2
-%                 m_irradiation_p(j,k) = sum(G_dir+G_dif+G_ref)/1e3;
-%             end
-%         end
-%     end
-% end
-% 
-% red_mount_l = m_irradiation_l(2,1);
-% green_mount_l = m_irradiation_l(1,2);
-% yellow_mount_l = m_irradiation_l(2,3);
-% blue_mount_l = m_irradiation_l(1,4);
-% 
-% landscapenis = red_mount_l + green_mount_l + yellow_mount_l + blue_mount_l
-% 
-% red_mount_p = m_irradiation_p(2,1);
-% green_mount_p = m_irradiation_p(1,2);
-% yellow_mount_p = m_irradiation_p(2,3);
-% blue_mount_p = m_irradiation_p(1,4);
-% 
-% portrate = red_mount_p + green_mount_p + yellow_mount_p + blue_mount_p
-
 
 %% Task 3 - PV Module Selection
 %Solar Tech TS60-6M3-280S 
